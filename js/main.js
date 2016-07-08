7d69e324d71050c7b5c5e46fb17e7623b6e191e5
Anirudh Mylavarapu <anirudhhimself@gmail.com>
2016-07-08 07:16:35 -0500

    Created custom style for button hover
    Also added a template that I plan on using later.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 // "use strict";
 // uh-oh global variable going to fix that soon.
+// (Probably with a closure)
 var howMany = 0;
 
 // I'll finish this function later
-var makeMoreButtons = function() {
+function sayHello() {
   howMany++;
-  for(int i = 0; i<howMany+1)
+  var template = document.querySelector('template');
+  var clone = document.importNode(template.content, true);
+  for (int i = 0; i < howMany + 1) {
+    document.body.appendChild(clone);
+  }
 
 };
