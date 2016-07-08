3dbe3c1c122f292bb829452896bb50d18cdc6675
Anirudh Mylavarapu <anirudhhimself@gmail.com>
2016-07-07 21:29:10 -0500

    Added JS base file and edited styles

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// "use strict";
+// uh-oh global variable going to fix that soon.
+var howMany = 0;
+
+// I'll finish this function later
+var makeMoreButtons = function() {
+  howMany++;
+  for(int i = 0; i<howMany+1)
+
+};
